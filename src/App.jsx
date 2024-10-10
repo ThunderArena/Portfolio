2b4ba05c59d44733eeecdfc627b7b477c3c1e887
Ankith <ankith06092004@gmail.com>
2024-10-10 10:54:23 +0530

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	deleted:    src/App.js 	new file:   src/App.jsx 	modified:   src/index.css 	new file:   src/pages/Home.jsx 	new file:   src/pages/Nav.jsx 	new file:   tailwind.config.js

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { NextUIProvider } from '@nextui-org/react';
+import Nav from './pages/Nav';
+import Home from './pages/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NextUIProvider>
+        <React.StrictMode>
+          <RouterProvider router={router} />
+        </React.StrictMode>
+      </NextUIProvider>
+    
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>
+      <Nav />
+      <Home />
+    </div>,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+export default App;
